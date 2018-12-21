5c962feb3b43e30213f356500a70ee0da747095a
Thaddeus Lim <thaddeuslzy@gmail.com>
2018-12-21 16:36:10 +0800

    Initial Commit

---
+++ Project_1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project_1
+//
+//  Created by Thaddeus Lim on 21/12/18.
+//  Copyright © 2018 Thaddeus Lim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
