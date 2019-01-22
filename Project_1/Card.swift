0b97452398e1ecf270c972e7e3720cc4b1c4bc8f
Thaddeus Lim <thaddeuslzy@gmail.com>
2019-01-22 22:45:11 +0800

    All updated changes until Lect 4

---
+++ Project_1/Card.swift
@@ -0,0 +1,9 @@
+//
+//  Card.swift
+//  Project_1
+//
+//  Created by Thaddeus Lim on 23/12/18.
+//  Copyright © 2018 Thaddeus Lim. All rights reserved.
+//
+
+import Foundation
---
+++ Project_1/Concentration.swift
@@ -0,0 +1,17 @@
+//
+//  Project_1_Model.swift
+//  Project_1
+//
+//  Created by Thaddeus Lim on 23/12/18.
+//  Copyright © 2018 Thaddeus Lim. All rights reserved.
+//
+
+import Foundation
+
+class Concentration {
+    var cards: Array<Card>
+    
+    func chooseCard(at index: Int) {
+         
+    }
+}
